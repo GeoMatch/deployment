fb0f8f1b1d8c3dab9a47151054d07e5b97014a52
Uday Jarajapu <udayj@stanford.edu>
2025-02-11 14:29:48 -0800

    fixed target_group_arn to be output

--- terraform/geomatch_app/lambda/outputs.tf
+++ terraform/geomatch_app/lambda/outputs.tf
@@ -7,3 +7,8 @@ output "function_name" {
   value       = aws_lambda_function.alb_lambda.function_name
   description = "Name of the Lambda function"
 }
+
+variable "target_group_arn" {
+  type        = string
+  description = "ARN of the ALB target group"
+}
--- terraform/geomatch_app/lambda/variables.tf
+++ terraform/geomatch_app/lambda/variables.tf
@@ -7,8 +7,3 @@ variable "environment" {
   type        = string
   description = "Environment name"
 }
-
-variable "target_group_arn" {
-  type        = string
-  description = "ARN of the ALB target group"
-}
