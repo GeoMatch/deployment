be64aec5869ded0de3471275cbf7171e550cf0a0
Uday Jarajapu <udayj@stanford.edu>
2025-02-11 15:01:07 -0800

    removed unwanted output

--- terraform/geomatch_app/lambda/outputs.tf
+++ terraform/geomatch_app/lambda/outputs.tf
@@ -7,8 +7,3 @@ output "function_name" {
   value       = aws_lambda_function.alb_lambda.function_name
   description = "Name of the Lambda function"
 }
-
-variable "target_group_arn" {
-  type        = string
-  description = "ARN of the ALB target group"
-}
