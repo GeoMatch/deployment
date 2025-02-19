9aa94ca78bb8efec8844d3c08eb82ea5bfa8dac1
Uday Jarajapu <udayj@stanford.edu>
2025-02-19 11:33:19 -0800

    added target_group arn to outputs

--- terraform/geomatch_app/alb/outputs.tf
+++ terraform/geomatch_app/alb/outputs.tf
@@ -9,3 +9,8 @@ output "alb_sg_arn" {
 output "alb_sg_id" {
   value = aws_security_group.alb.id
 }
+
+output "target_group_arn" {
+  value = aws_lb_target_group.this.arn
+  description = "ARN of the target group"
+}
