b1b28fbc5bc0b8887505dc2e5747eb40e6b87e6c
Uday Jarajapu <udayj@stanford.edu>
2025-02-24 11:59:50 -0800

    fixed typos for uat

--- terraform/geomatch_app/alb/main.tf
+++ terraform/geomatch_app/alb/main.tf
@@ -101,7 +101,7 @@ resource "aws_alb" "uat" {
   tags = {
     Project     = var.project
     Environment = var.environment
-    Name        = local.name_prefix
+    Name        = "${local.name_prefix}-uat"
   }
 }
 
@@ -117,7 +117,7 @@ resource "aws_lb_target_group" "uat" {
 }
 
 resource "aws_lb_listener" "https-uat" {
-  load_balancer_arn = aws_alb.this.arn
+  load_balancer_arn = aws_alb.uat.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
