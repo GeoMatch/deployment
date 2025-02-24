2a406eb23fae453d74c95c5140bb3092e4487563
Uday Jarajapu <udayj@stanford.edu>
2025-02-24 09:29:20 -0800

    fixed reference to ARN in lb_target_group

--- terraform/geomatch_app/alb/outputs.tf
+++ terraform/geomatch_app/alb/outputs.tf
@@ -11,7 +11,7 @@ output "alb_sg_id" {
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.alb.arn
+  value = aws_lb_target_group.this.arn
   description = "ARN of the target group"
 }
 
