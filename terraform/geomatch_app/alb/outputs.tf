4d92158376efd3800324714356bea6ea5e314686
Uday Jarajapu <udayj@stanford.edu>
2025-02-25 17:02:21 -0800

    conditional check to output

--- terraform/geomatch_app/alb/outputs.tf
+++ terraform/geomatch_app/alb/outputs.tf
@@ -3,7 +3,7 @@ output "alb_arn" {
 }
 
 output "uat_alb_arn" {
-  value = aws_alb.uat[0].arn
+  value = var.require_cardinal_cloud_auth ? aws_alb.uat[0].arn : null
 }
 
 output "alb_sg_arn" {
@@ -15,12 +15,12 @@ output "alb_sg_id" {
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.uat[0].arn
+  value = var.require_cardinal_cloud_auth ? aws_lb_target_group.uat[0].arn : null
   description = "ARN of the target group"
 }
 
 output "listener_arn" {
-  value = aws_lb_listener.https-uat[0].arn
+  value = var.require_cardinal_cloud_auth ? aws_lb_listener.https-uat[0].arn : null
   description = "ARN of the listener"
 }
 
